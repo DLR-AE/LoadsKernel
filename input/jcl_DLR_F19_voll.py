1bd4aa611c4ff828b0354a70b4fe46c759ef4248
Voß, Arne <arne.voss@dlr.de>
2015-03-05 11:00:23 +0100

    Update in JCL auf neue Modellversion

--- input/jcl_DLR_F19_voll.py
+++ input/jcl_DLR_F19_voll.py
@@ -21,35 +21,37 @@ class jcl:
                         'MAC_ref': [6.0, 0.0, 0.0],
                        }
         self.geom = {'method': 'mona',
-                     'filename_grid':['/scratch/DLR-F19-S_150126/mg02_DLR-F19-S/output/mg02_DLR-F19-S_baseline.GRID',
-                                      '/scratch/DLR-F19-S_150126/mg05_DLR-F19-S_LinkeSeite/output/mg05_DLR-F19-S_baseline.GRID',
-                                      '/scratch/DLR-F19-S_150126/mg03_DLR-F19-S_Klappen/output/Klappen_innen.GRID',
-                                      '/scratch/DLR-F19-S_150126/mg03_DLR-F19-S_Klappen/output/Klappen_aussen.GRID',
-                                      '/scratch/DLR-F19-S_150126/mg03_DLR-F19-S_Klappen_LinkeSeite/output/Klappen_innen.GRID',
-                                      '/scratch/DLR-F19-S_150126/mg03_DLR-F19-S_Klappen_LinkeSeite/output/Klappen_aussen.GRID'],
-                     #'filename_KAA':'/scratch/DLR-F19-S_150126/assembly_DLR-F-19-S/matrix_export/KAA.dat',
+                     'filename_grid':['/scratch/DLR-F19-S_150217/mg02_DLR-F19-S/output/mg02_DLR-F19-S_baseline.GRID',
+                                      '/scratch/DLR-F19-S_150217/mg05_DLR-F19-S_LinkeSeite/output/mg05_DLR-F19-S_baseline.GRID',
+                                      '/scratch/DLR-F19-S_150217/mg03_DLR-F19-S_Klappen/output/Klappen_innen.GRID',
+                                      '/scratch/DLR-F19-S_150217/mg03_DLR-F19-S_Klappen/output/Klappen_aussen.GRID',
+                                      '/scratch/DLR-F19-S_150217/mg03_DLR-F19-S_Klappen_LinkeSeite/output/Klappen_innen.GRID',
+                                      '/scratch/DLR-F19-S_150217/mg03_DLR-F19-S_Klappen_LinkeSeite/output/Klappen_aussen.GRID'],
+                     #'filename_KAA':'/scratch/DLR-F19-S_150217/assembly_DLR-F-19-S/matrix_export/KAA.dat',
                     }
         self.aero = {'method': 'mona_steady',
                      'key':['VC', 'MC', 'VD', 'MD'],
                      'Ma': [0.8, 0.9, 0.89, 0.97],
-                     'filename_caero_bdf': ['/scratch/DLR-F19-S_150126/mg02_DLR-F19-S/output/mg02_DLR-F19-S_baseline.CAERO1_bdf', 
-                                            '/scratch/DLR-F19-S_150126/mg05_DLR-F19-S_LinkeSeite/output/mg05_DLR-F19-S_baseline.CAERO1_bdf'],
-                     'filename_deriv_4_W2GJ': ['/scratch/DLR-F19-S_150126/mg02_DLR-F19-S/output/mg02_DLR-F19-S_baseline.deriv_4_W2GJ', 
-                                               '/scratch/DLR-F19-S_150126/mg05_DLR-F19-S_LinkeSeite/output/mg05_DLR-F19-S_baseline.deriv_4_W2GJ'],
-                     'filename_aesurf': ['/scratch/DLR-F19-S_150126/mg02_DLR-F19-S/output/mg02_DLR-F19-S_baseline.AESURF', 
-                                         '/scratch/DLR-F19-S_150126/mg05_DLR-F19-S_LinkeSeite/output/mg05_DLR-F19-S_baseline.AESURF'],
-                     'filename_aelist': ['/scratch/DLR-F19-S_150126/mg02_DLR-F19-S/output/mg02_DLR-F19-S_baseline.AELIST', 
-                                         '/scratch/DLR-F19-S_150126/mg05_DLR-F19-S_LinkeSeite/output/mg05_DLR-F19-S_baseline.AELIST'],
-                     'filename_AIC': ['/scratch/DLR-F19-S_150126/trim_DLR-F19-S/aic/AJJ01.dat', \
-                                      '/scratch/DLR-F19-S_150126/trim_DLR-F19-S/aic/AJJ02.dat', \
-                                      '/scratch/DLR-F19-S_150126/trim_DLR-F19-S/aic/AJJ03.dat', \
-                                      '/scratch/DLR-F19-S_150126/trim_DLR-F19-S/aic/AJJ04.dat', \
+                     'filename_caero_bdf': ['/scratch/DLR-F19-S_150217/mg02_DLR-F19-S/output/mg02_DLR-F19-S_baseline.CAERO1_bdf', 
+                                            '/scratch/DLR-F19-S_150217/mg05_DLR-F19-S_LinkeSeite/output/mg05_DLR-F19-S_baseline.CAERO1_bdf'],
+                     'filename_deriv_4_W2GJ': ['/scratch/DLR-F19-S_150217/mg02_DLR-F19-S/output/mg02_DLR-F19-S_baseline.deriv_4_W2GJ', 
+                                               '/scratch/DLR-F19-S_150217/mg05_DLR-F19-S_LinkeSeite/output/mg05_DLR-F19-S_baseline.deriv_4_W2GJ'],
+                     'filename_aesurf': ['/scratch/DLR-F19-S_150217/mg02_DLR-F19-S/output/mg02_DLR-F19-S_baseline.AESURF', 
+                                         '/scratch/DLR-F19-S_150217/mg05_DLR-F19-S_LinkeSeite/output/mg05_DLR-F19-S_baseline.AESURF'],
+                     'filename_aelist': ['/scratch/DLR-F19-S_150217/mg02_DLR-F19-S/output/mg02_DLR-F19-S_baseline.AELIST', 
+                                         '/scratch/DLR-F19-S_150217/mg05_DLR-F19-S_LinkeSeite/output/mg05_DLR-F19-S_baseline.AELIST'],
+                     'filename_AIC': ['/scratch/DLR-F19-S_150217/trim_DLR-F19-S/aic/AJJ01.dat', \
+                                      '/scratch/DLR-F19-S_150217/trim_DLR-F19-S/aic/AJJ02.dat', \
+                                      '/scratch/DLR-F19-S_150217/trim_DLR-F19-S/aic/AJJ03.dat', \
+                                      '/scratch/DLR-F19-S_150217/trim_DLR-F19-S/aic/AJJ04.dat', \
                                      ],
                     }
         self.mass = {'method': 'mona',
-                       'key': ['M'],
-                       #'filename_MAA':['/scratch/DLR-F19-S_150126/assembly_DLR-F-19-S/matrix_export/MAA_M.dat'],
-                       'filename_S103':['/scratch/DLR-F19-S_150126/assembly_DLR-F-19-S/nastran/dim_crosscheck_SOL103_M.f06'], 
+                       'key': ['M', 'BFDM'],
+                       #'filename_MAA':['/scratch/DLR-F19-S_150217/assembly_DLR-F-19-S/matrix_export/MAA_M.dat'],
+                       'filename_S103':['/scratch/DLR-F19-S_150217/assembly_DLR-F-19-S/nastran/assembly_SOL103_M.f06', 
+                                        '/scratch/DLR-F19-S_150217/assembly_DLR-F-19-S/nastran/assembly_SOL103_BFDM.f06',
+                                       ], 
                        'omit_rb_modes': True, 
                        'modes':np.arange(1,16),                        
                       }
@@ -57,24 +59,24 @@ class jcl:
                      'key':['FL000','FL055', 'FL075', 'FL300', 'FL450'],
                      'h': ft2m([0, 5500, 7500, 30000, 45000]),
                     }
-        self.trimcase = [{'desc': 'CC.M.OVCFL000.LLFLevel', 
-                          'manoeuver': 'LLFLevel', 
-                          'Ma': 0.8, 
-                          'aero': 'VC', 
-                          'altitude': 'FL000', 
-                          'mass': 'M',
-                          'Nz': 1.0, 
-                          'p': 0.0,
-                          'q': 0.0, 
-                          'pdot': 0.0, 
-                          'qdot': 0.0, 
-                        }]
+#        self.trimcase = [{'desc': 'CC.M.OVCFL000.LLFLevel', 
+#                          'manoeuver': 'LLFLevel', 
+#                          'Ma': 0.8, 
+#                          'aero': 'VC', 
+#                          'altitude': 'FL000', 
+#                          'mass': 'M',
+#                          'Nz': 1.0, 
+#                          'p': 0.0,
+#                          'q': 0.0, 
+#                          'pdot': 0.0, 
+#                          'qdot': 0.0, 
+#                        }]
                         
-#        from numpy import array
-#        fid = open('/scratch/DLR-F19-S_150217/trim_DLR-F19-S/test_trim.trimcase_dict', 'r')
-#        trimcase_str = fid.read()
-#        fid.close()
-#        self.trimcase = eval(trimcase_str)
+        from numpy import array
+        fid = open('/scratch/DLR-F19-S_150217/trim_DLR-F19-S/test_trim.trimcase_dict', 'r')
+        trimcase_str = fid.read()
+        fid.close()
+        self.trimcase = eval(trimcase_str)
         
         # End
 
