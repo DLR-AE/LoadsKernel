438c973f3f4fcd1a297926c69662a1e502f4875a
Voß, Arne <arne.voss@dlr.de>
2015-07-22 10:14:27 +0200

    Anpassungen im Programmablauf, Anzeige des aktuellen Trimcases

--- kernel/kernel.py
+++ kernel/kernel.py
@@ -18,15 +18,12 @@ def run_kernel(job_name, pre=False, main=False, post=False, test=False):
     print 'post: ' + str(post)
     print 'test: ' + str(test)
 
-    from trim import trim
-    from model import model as model_obj
-    from post_processing import post_processing
-    
     print '--> Reading parameters from JCL.'
     jcl = imp.load_source('jcl', '../input/' + job_name + '.py')
     jcl = jcl.jcl() 
         
     if pre: 
+        from model import model as model_obj
         print '--> Starting preprocessing.'   
         t_start = time.time()
         model = model_obj(jcl)
@@ -41,6 +38,7 @@ def run_kernel(job_name, pre=False, main=False, post=False, test=False):
         print '--> Done in %.2f [sec].' % (time.time() - t_start)
         
     if main:
+        from trim import trim
         if not 'model' in locals():
             model = load_model(job_name)
         
@@ -51,6 +49,8 @@ def run_kernel(job_name, pre=False, main=False, post=False, test=False):
             print ''
             print '========================================' 
             print 'trimcase: ' + jcl.trimcase[i]['desc']
+            print 'subcase: ' + str(jcl.trimcase[i]['subcase'])
+            print '(case ' +  str(i+1) + ' of ' + str(len(jcl.trimcase)) + ')' 
             print '========================================' 
             t_start = time.time()
             trim_i = trim(model, jcl, jcl.trimcase[i])
@@ -65,6 +65,7 @@ def run_kernel(job_name, pre=False, main=False, post=False, test=False):
         print '--> Done in %.2f [sec].' % (time.time() - t_start)
     
     if post:
+        from post_processing import post_processing as post_processing_obj
         if not 'model' in locals():
             model = load_model(job_name)
         
@@ -75,18 +76,24 @@ def run_kernel(job_name, pre=False, main=False, post=False, test=False):
         
         print '--> Starting Post for %d trimcase(s).' % len(jcl.trimcase)
         t_start = time.time()
-        post_processing = post_processing(jcl, model, response)
+        post_processing = post_processing_obj(jcl, model, response)
         post_processing.force_summation_method()
         post_processing.cuttingforces()
+        post_processing.gather_monstations() # wird zum plotten benoetigt
         print '--> Done in %.2f [sec].' % (time.time() - t_start)
         
         print '--> Saving response(s) and monstations.'  
         with open('../output/response_' + job_name + '.pickle', 'w') as f:
             cPickle.dump(response, f, cPickle.HIGHEST_PROTOCOL)
+        with open('../output/monstations_' + job_name + '.pickle', 'w') as f:
+            cPickle.dump(post_processing.monstations, f, cPickle.HIGHEST_PROTOCOL)
+            
+        print '--> Saving auxiliary output data.'  
         post_processing.save_monstations('../output/monstations_' + job_name + '.bdf')     
         post_processing.save_nodalloads('../output/nodalloads_' + job_name + '.bdf')
         post_processing.save_nodaldefo('../output/nodaldefo_' + job_name)
-        #post_processing.gather_monstations() # wird zum plotten benoetigt
+        
+        
         #post_processing.plot_monstations(post_processing.monstations, '../output/monstations_' + job_name + '.pdf')
         #post_processing.plot_forces_deformation_interactive()
 
@@ -98,7 +105,7 @@ def run_kernel(job_name, pre=False, main=False, post=False, test=False):
             print '--> Loading response(s).'  
             with open('../output/response_' + job_name + '.pickle', 'r') as f:
                 response = cPickle.load(f)
-                
+        print 'bla'   
 #        import pprint
 #        np.set_printoptions(threshold=np.inf)
 #        offsets_l = model.aerogrid['offset_l']     
@@ -131,7 +138,7 @@ def run_kernel(job_name, pre=False, main=False, post=False, test=False):
 #            write_force_and_moment_cards(fid, model.strcgrid, force_vector, 1)
         
         
-        print 'Done.'
+    print 'AE Kernel finished.'
 
 def load_model(job_name):
     print '--> Loading model data.'
@@ -144,7 +151,9 @@ def load_model(job_name):
 if __name__ == "__main__":
     #run_kernel('jcl_DLR_F19_voll', pre=True, main = True, post = True)
     #run_kernel('jcl_DLR_F19_voll_CFD', main = True)
-    run_kernel('jcl_DLR_F19_voll_2', post=True)
-    
-    
-   
+    #run_kernel('jcl_DLR_F19_voll_2', post=True)
+    #run_kernel('jcl_DLR_F19_manloads', pre=True)
+    #run_kernel('jcl_DLR_F19_manloads', main=True)
+    #run_kernel('jcl_DLR_F19_manloads', post=True)
+    #run_kernel('jcl_DLR_F19_manloads', test=True)
+    run_kernel('jcl_DLR_F19_subcase109', main=True)
