BUG: Berechnung der Anstroemung der Steuerflaechen falsch, nur noch Rotation um y-achse wird beruecksichtigt! -> vorerst kein Seitenruder...

kernel/model_equations.py
@@ -192,7 +192,8 @@ class nastran:
                 #for i_N in self.model.aerogrid['N']:
                 #    N_rot.append(np.dot(drehmatrix_N, i_N))
                 #N_rot = np.array(N_rot)
-                wjx2 = np.sin(Ujx2[self.model.aerogrid['set_j'][:,(3)]]) + np.sin(Ujx2[self.model.aerogrid['set_j'][:,(4)]]) + np.sin(Ujx2[self.model.aerogrid['set_j'][:,(5)]])  #* Vtas/Vtas
+                wjx2 = np.sin(Ujx2[self.model.aerogrid['set_j'][:,(4)]])  #* Vtas/Vtas
+                #wjx2 = np.sin(Ujx2[self.model.aerogrid['set_j'][:,(3)]]) + np.sin(Ujx2[self.model.aerogrid['set_j'][:,(4)]]) + np.sin(Ujx2[self.model.aerogrid['set_j'][:,(5)]])  #* Vtas/Vtas
                 flx2 = self.q_dyn * self.model.aerogrid['N'].T*self.model.aerogrid['A']*np.dot(Qjj, wjx2)
                 #fjx2 = q_dyn * N_rot.T*self.model.aerogrid['A']*np.dot(self.model.aero['Qjj'][i_aero], wjx2)
                 Plx2 = np.zeros(np.shape(Plx1))