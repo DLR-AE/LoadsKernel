b904f70198e82627731e649d4f1e30648718d808
Voß, Arne <arne.voss@dlr.de>
2016-04-29 13:25:52 +0200

    Erweiterung des EFCS fuer den Discus2c auf das Berechnen von Steuerkommandos (dcommand) auf Basis von gemessenen Zeitsignalen der Steuerflaechen

--- kernel/efcs.py
+++ kernel/efcs.py
@@ -5,6 +5,7 @@ Created on Tue Mar  3 14:36:41 2015
 @author: voss_ar
 """
 import numpy as np
+import csv
 
 class mephisto:
     def __init__(self):
@@ -116,28 +117,63 @@ class allegra:
 class discus2c:
     def __init__(self):
         self.keys = ['AIL_Rin', 'AIL_Rout', 'AIL_Lin', 'AIL_Lout', 'ELEV_R', 'ELEV_L', 'RUDD' ]
-        self.Ux2_0     = np.array([0.0,     0.0,   0.0,   0.0,   0.0,   0.0,   0.0])
-        self.Ux2_lower = np.array([-30.0, -30.0, -30.0, -30.0, -30.0, -30.0, -30.0])/180*np.pi
-        self.Ux2_upper = np.array([ 30.0,  30.0,  30.0,  30.0,  30.0,  30.0,  30.0])/180*np.pi
+        self.Ux2_0 = np.array([0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0])
+        self.Ux2_lower = np.array([-30.0, -30.0, -30.0, -30.0, -30.0, -30.0, -30.0]) / 180 * np.pi
+        self.Ux2_upper = np.array([ 30.0, 30.0, 30.0, 30.0, 30.0, 30.0, 30.0]) / 180 * np.pi
         
-        self.alpha_lower = -10.0/180*np.pi
-        self.alpha_upper =  10.0/180*np.pi
+        self.alpha_lower = -10.0 / 180 * np.pi
+        self.alpha_upper = 10.0 / 180 * np.pi
+    
+    def cs_signal_init(self, case_desc):
+        cases = ['FT-P1', 'FT-P2', 'FT-P3', 'Elev3211_A', 'Elev3211_B']
+        files = ['/scratch/Discus2c_Data+Info/FT-P1_Pilot1.txt',
+                 '/scratch/Discus2c_Data+Info/FT-P2_Pilot2.txt',
+                 '/scratch/Discus2c_Data+Info/FT-P3_Pilot1.txt',
+                 '/scratch/Discus2c_Data+Info/Elev3211_2-15A_Pilot1.txt',
+                 '/scratch/Discus2c_Data+Info/Elev3211_2-15B_Pilot1.txt']
+        tstart = [80.0, 190.0, 168.0, 911.0, 933.0]
+        
+        if case_desc not in cases:
+            print 'Warning, no time signal for cs deflections found!'
+            
+        with open(files[cases.index(case_desc)]) as csvfile:
+            reader = csv.DictReader(csvfile, dialect='excel-tab')
+            reader.next()  # skip second line of file
+            data = []
+            for row in reader:
+                data.append([float(row['Time        ']), float(row['xi_l_corr   '])/180.0*np.pi, float(row['xi_r_corr   '])/180.0*np.pi, float(row['eta_corr    '])/180.0*np.pi, float(row['zeta_corr   '])/180.0*np.pi])
+        self.data = np.array(data)
+        self.tstart = tstart[cases.index(case_desc)]
+        
+    def cs_signal(self, t):
+        line0 = np.argmin(np.abs(self.data[:,0] - t - self.tstart))
+        line1 = line0 + 1
+        
+        t0 = self.data[line0,0]
+        t1 = self.data[line1,0]
+        
+        dxi = + 0.5 * (self.data[line1,1] - self.data[line0,1]) / (t1 - t0) \
+              - 0.5 * (self.data[line1,2] - self.data[line0,2]) / (t1 - t0)
+        deta = - (self.data[line1,3] - self.data[line0,3]) / (t1 - t0)
+        dzeta = (self.data[line1,4] - self.data[line0,4]) / (t1 - t0)
+
+        return [dxi, deta, dzeta]
                 
     def efcs(self, command_xi, command_eta, command_zeta):
 
         # Ausgangsposition
-        delta_AIL_Rin  = self.Ux2_0[0]
+        delta_AIL_Rin = self.Ux2_0[0]
         delta_AIL_Rout = self.Ux2_0[1]
-        delta_AIL_Lin  = self.Ux2_0[2]
+        delta_AIL_Lin = self.Ux2_0[2]
         delta_AIL_Lout = self.Ux2_0[3]
-        delta_ELEV_R   = self.Ux2_0[4]
-        delta_ELEV_L   = self.Ux2_0[5]
-        delta_RUDD     = self.Ux2_0[6]
+        delta_ELEV_R = self.Ux2_0[4]
+        delta_ELEV_L = self.Ux2_0[5]
+        delta_RUDD = self.Ux2_0[6]
         
         # xi - Rollachse
-        delta_AIL_Rin  -= command_xi
+        delta_AIL_Rin -= command_xi
         delta_AIL_Rout -= command_xi
-        delta_AIL_Lin  += command_xi
+        delta_AIL_Lin += command_xi
         delta_AIL_Lout += command_xi
         
         # eta - Nickachse
@@ -145,7 +181,7 @@ class discus2c:
         delta_ELEV_L -= command_eta
         
         # zeta - Gierachse
-        delta_RUDD = command_zeta
+        delta_RUDD -= command_zeta # bei negativem zeta (rechts treten) soll das Ruder nach rechts ausschlagen, siehe CORD 321
         
         Ux2 = np.array([delta_AIL_Rin, delta_AIL_Rout, delta_AIL_Lin, delta_AIL_Lout, delta_ELEV_R, delta_ELEV_L, delta_RUDD])
         
