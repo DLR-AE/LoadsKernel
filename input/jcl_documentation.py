5aaf2275a5d92f3a93aeb8dcc4f5c1b33bcad0a9
Voß, Arne <arne.voss@dlr.de>
2017-05-12 16:12:54 +0200

    Update of JCL documentation file

--- input/jcl_documentation.py
+++ input/jcl_documentation.py
@@ -26,18 +26,26 @@ class jcl:
                      'filename_GM': 'GM.dat',       # matrix GM via DMAP Alter und OP4            - required for mass method = 'modalanalysis' or 'guyan'
                      'filename_aset': 'aset.bdf',   # bdf file(s) with ASET1-card                 - required for mass method = 'guyan'
                     }
-        self.aero = {'method': 'mona_steady', # 'mona_steady' or 'hybrid'
+        self.aero = {'method': 'mona_steady', # 'mona_steady', 'mona_unsteady', 'steady_nonlin' or 'hybrid'
                      'flex': True, # True or False, aerodynamic feedback of elastic structure
-                     'method_caero': 'CAERO1',                              # aerogrid is given by CAERO1 or by CQUAD4 cards
-                     'filename_caero_bdf': ['CAERO1_bdf'],                  # bdf file(s) with CAERO1 or CQUAD4-cards for aerogrid. IDs in ascending order.
-                     'filename_deriv_4_W2GJ': ['filename.deriv_4_W2GJ'],    # ModGen output for camber and twist correction. Same order as the aerogrid.
-                     'filename_aesurf': ['filename.AESURF'],                # bdf file(s) with AESURF-cards
-                     'filename_aelist': ['filename.AELIST'],                # bdf file(s) with AELIST-cards
-                     'hingeline': 'z',                                      # The hingeline of a CS is given by a CORD. Either the y- or the z-axis is taken as hingeline. 'y', 'z' 
-                     'method_AIC': 'vlm', # 'vlm' or 'nastran'
+                     'method_caero': 'CAERO1',                               # aerogrid is given by CAERO1, CAERO7 or by CQUAD4 cards
+                     'filename_caero_bdf': ['CAERO1_bdf'],                   # bdf file(s) with CAERO1 or CQUAD4-cards for aerogrid. IDs in ascending order.
+                     'filename_deriv_4_W2GJ': ['filename.deriv_4_W2GJ'],     # ModGen output for camber and twist correction. Same order as the aerogrid.
+                     'filename_aesurf': ['filename.AESURF'],                 # bdf file(s) with AESURF-cards
+                     'filename_aelist': ['filename.AELIST'],                 # bdf file(s) with AELIST-cards
+                     'hingeline': 'z',                                       # The hingeline of a CS is given by a CORD. Either the y- or the z-axis is taken as hingeline. 'y', 'z' 
+                     'method_AIC': 'vlm',                                    # 'vlm' (internal), 'dlm' (octave) or 'nastran' (external form matrices)
                      'key':['VC', 'MC'],
                      'Ma': [0.8, 0.9],
-                     'filename_AIC': ['AIC_VC.dat', 'AIC_MC.dat'],  # provide OP4 files with AICs if method_AIC = 'nastran'
+                     'filename_AIC': ['AIC_VC.dat', 'AIC_MC.dat'],           # provide OP4 files with AICs if method_AIC = 'nastran'
+                     'k_red': [0.001, 0.01, 0.03, 0.1, 0.3, 0.6, 1.0, 1.5 ], # reduced frequencies for DLM, Nastran Definition!
+                     'n_poles': 4,                                           # number of poles for rational function approximation (RFA)
+                     'Cn_beta_corr': [ -0.012],                              # Correction coefficient at CG, negativ = destabilizing
+                     'Cm_alpha_corr':[ 0.22],                                # Correction coefficient at CG, positiv = destabilizing
+                     'viscous_drag': 'coefficients',                         # Correction coefficient at MAC, Cd = Cd0 + dCd/dalpha^2 * alpha^2
+                     'Cd_0': [0.005],
+                     'Cd_alpha^2': [0.018*6.28**2.0], 
+                     'induced_drag':True,                                    # True or False, calculates local induced drag e.g. for roll-yaw-coupling
                     }
         self.matrix_aerodb = {} # if aero method = 'hybrid'
         self.matrix_aerodb['alpha'] = {} # build aerodynamic database for 'alpha' or control surface deflections 'AIL-S1/2/3/4'
@@ -71,6 +79,25 @@ class jcl:
                      'key':['FL000','FL055', 'FL075', 'FL200', 'FL300', 'FL450'],
                      'h': ft2m([0, 5500, 7500, 20000, 30000, 45000]), # altitude in meters
                     }
+        self.eom = {'version': 'linear'} # 'linear, 'waszak'
+        
+        # parameters for generic landing gear, see PhD Thesis of Wolf Krueger and Sunpeth Cumnuantip
+        para_LG = {'stroke_length':  0.3,   # m
+                    'fitting_length': 0.72, # m
+                    'n':  1.4,
+                    'ck': 1.0,
+                    'd2':  85000.0,         # N/(m/s)^2
+                    'F_static': 53326.0,    # N
+                    'r_tire': 0.28,         # m
+                    'c1_tire': 832000.0,    # N/m
+                    'd1_tire': 4500.0,      # N/(m/s)^2
+                    'm_tire': 58.0,         # kg
+                  }
+        self.landinggear= {'method': 'generic',                         # activates a generic landing gear model during time simulation
+                           'key': ['MLG1', 'MLG2', 'NLG' ],
+                           'attachment_point':[800002, 800003, 800001], # IDs of FE attachment nodes
+                           'para': [para_LG, para_LG, para_LG],         # parameters for generic landing gear module, see above
+                          }
         self.trimcase = [{'desc': 'CC.BFDM.OVCFL000.Vergleichsfall53', # description of maneuver case, e.g. according to G. Pinho Chiozzotto, "Kriterien fuer die Erstellung eines Lastenkatalogs," Institute of Aeroelasticity, iLOADs MS1.2, Feb. 2014.
                           'manoeuver': '',      # unused
                           'subcase': 53,        # ID number
@@ -97,7 +124,8 @@ class jcl:
                          'gust_orientation': 0, # orientation of the gust in [deg], 0/360 = gust from bottom, 180 = gust from top
                          'gust_para':{'Z_mo': 12500.0, 'MLW': 65949.0, 'MTOW': 73365.0, 'MZFW': 62962.0, 'MD': 0.87, 'T1': 0.00}, # gust parameters according to CS-25
                          'cs_signal': False,    # True or False, allows playback of control surface signals via efcs
-                         'controller': False    # True or False, enables a generic controller e.g. to maintain p, q and r
+                         'controller': False,   # True or False, enables a generic controller e.g. to maintain p, q and r
+                         'landinggear':False,   # True or False, enables a generic landing gear
                         },
                        ] 
         # End
