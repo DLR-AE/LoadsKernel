0e1134febe3d4343fa139ae90a879081a517c8b9
Voß, Arne <arne.voss@dlr.de>
2019-02-14 13:53:29 +0100

    Adapted launch script to example with Discus2c

--- input/launch.py
+++ input/launch.py
@@ -2,18 +2,19 @@
 
 import sys
 
+# Here you add the location of the Loads Kernel
+sys.path.append("../../loads-kernel")
+sys.path.append("/opt/tau/bin/py_el")
+
 from kernel import kernel
 from compare import compare
 from modelviewer import modelviewer
 
-# Here you add the location of the Loads Kernel
-sys.path.append("/scratch/loads-kernel")
-sys.path.append("/opt/tau/bin/py_el")
 
 # Here you launch the Loads Kernel with your job
-k = kernel.Kernel('jcl_Discus2c_CoFE', pre=True, main=True, post=True, test=False, parallel=False,
-                  path_input='/scratch/Discus2c/JCLs',
-                  path_output='/scratch/test')
+k = kernel.Kernel('jcl_Discus2c', pre=True, main=True, post=True, test=False, parallel=False,
+                  path_input='../../loads-kernel-examples/Discus2c/JCLs',
+                  path_output='../../loads-kernel-examples/Discus2c/output')
 k.run()
 
 # Loads Compare
