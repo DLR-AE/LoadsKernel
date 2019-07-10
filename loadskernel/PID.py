7dfd28036fb5849839f5308ced31ae436a213bae
Voß, Arne <arne.voss@dlr.de>
2019-07-10 10:04:55 +0200

    PID-T1 Regelungsglied implementiert

--- loadskernel/PID.py
+++ loadskernel/PID.py
@@ -118,3 +118,52 @@ class PID_standart:
             self.last_error = error
 
             self.output = self.Kp * (self.PTerm + (1.0/self.Ti * self.ITerm) + (self.Td * self.DTerm))
+
+
+class PID_T1(PID_standart):
+    def __init__(self, Kp=0.2, Ti=0.0, Td=0.0, T1=0.0, t=0.0):
+        self.Kp = Kp
+        self.Ti = Ti
+        self.Td = Td
+        self.T1 = T1
+        self.sample_time = 0.00
+        self.current_time = t
+        self.last_time = self.current_time
+
+        self.clear()
+        
+    def update(self, t, feedback_value):
+        """Calculates PID value for given reference feedback
+        .. math::
+            u(t) = K_p * [ e(t) + 1/T_i \int_{0}^{t} e(t)dt + T_d {de}/{dt} ]
+        """
+        error = self.SetPoint - feedback_value
+
+        self.current_time = t
+        delta_time = self.current_time - self.last_time
+        delta_error = error - self.last_error
+
+        if (delta_time >= self.sample_time):
+            self.PTerm = error
+            self.ITerm += error * delta_time
+
+            if (self.ITerm < -self.windup_guard):
+                self.ITerm = -self.windup_guard
+                #print 'windup'
+            elif (self.ITerm > self.windup_guard):
+                self.ITerm = self.windup_guard
+                #print 'windup'
+
+            self.DTerm = 0.0
+            if delta_time > 0:
+                self.DTerm = delta_error / delta_time
+
+            # Remember last time and last error for next calculation
+            self.last_time = self.current_time
+            self.last_error = error
+
+            self.output = self.Kp * ((self.T1+self.Ti)/self.Ti*self.PTerm + (1.0/self.Ti * self.ITerm) + (self.T1 + self.Td) * self.DTerm)
+    
+    
+    
+    
