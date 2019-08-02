7639aa18f9edc0e83667843d92cd549148ec68a1
Voß, Arne <arne.voss@dlr.de>
2019-08-02 15:12:37 +0200

    Removed duplicate lines of code form PID class

--- loadskernel/PID.py
+++ loadskernel/PID.py
@@ -1,14 +1,12 @@
 
-class PID_ideal:
+class PID_ideal():
     def __init__(self, Kp=0.2, Ki=0.0, Kd=0.0, t=0.0):
         self.Kp = Kp
         self.Ki = Ki
         self.Kd = Kd
-
-        self.sample_time = 0.00
+        self.sample_time = 0.0
         self.current_time = t
         self.last_time = self.current_time
-
         self.clear()
 
     def clear(self):
@@ -28,70 +26,8 @@ class PID_ideal:
     def setSetPoint(self, SetPoint):
         self.SetPoint = SetPoint
         
-    def update(self, t, feedback_value):
-        """Calculates PID value for given reference feedback
-        .. math::
-            u(t) = K_p e(t) + K_i \int_{0}^{t} e(t)dt + K_d {de}/{dt}
-        """
-        error = self.SetPoint - feedback_value
-
-        self.current_time = t
-        delta_time = self.current_time - self.last_time
-        delta_error = error - self.last_error
-
-        if (delta_time >= self.sample_time):
-            self.PTerm = self.Kp * error
-            self.ITerm += error * delta_time
-
-            if (self.ITerm < -self.windup_guard):
-                self.ITerm = -self.windup_guard
-            elif (self.ITerm > self.windup_guard):
-                self.ITerm = self.windup_guard
-
-            self.DTerm = 0.0
-            if delta_time > 0:
-                self.DTerm = delta_error / delta_time
-
-            # Remember last time and last error for next calculation
-            self.last_time = self.current_time
-            self.last_error = error
-
-            self.output = self.PTerm + (self.Ki * self.ITerm) + (self.Kd * self.DTerm)
-
-class PID_standart:
-    def __init__(self, Kp=0.2, Ti=0.0, Td=0.0, t=0.0):
-        self.Kp = Kp
-        self.Ti = Ti
-        self.Td = Td
-        self.sample_time = 0.00
-        self.current_time = t
-        self.last_time = self.current_time
-
-        self.clear()
-
-    def clear(self):
-        """Clears PID computations and coefficients"""
-        self.SetPoint = 0.0
-
-        self.PTerm = 0.0
-        self.ITerm = 0.0
-        self.DTerm = 0.0
-        self.last_error = 0.0
-
-        # Windup Guard
-        self.int_error = 0.0
-        self.windup_guard = 20.0
-
-        self.output = 0.0
-    
-    def setSetPoint(self, SetPoint):
-        self.SetPoint = SetPoint
+    def update_PID_Terms(self, t, feedback_value):
         
-    def update(self, t, feedback_value):
-        """Calculates PID value for given reference feedback
-        .. math::
-            u(t) = K_p * [ e(t) + 1/T_i \int_{0}^{t} e(t)dt + T_d {de}/{dt} ]
-        """
         error = self.SetPoint - feedback_value
 
         self.current_time = t
@@ -104,10 +40,8 @@ class PID_standart:
 
             if (self.ITerm < -self.windup_guard):
                 self.ITerm = -self.windup_guard
-                #print 'windup'
             elif (self.ITerm > self.windup_guard):
                 self.ITerm = self.windup_guard
-                #print 'windup'
 
             self.DTerm = 0.0
             if delta_time > 0:
@@ -116,11 +50,40 @@ class PID_standart:
             # Remember last time and last error for next calculation
             self.last_time = self.current_time
             self.last_error = error
+    
+    def update(self, t, feedback_value):
+        self.update_PID_Terms(t, feedback_value)
+        self.update_output()
+            
+    def update_output(self):
+        """Calculates PID value for given reference feedback
+        .. math::
+            u(t) = K_p e(t) + K_i \int_{0}^{t} e(t)dt + K_d {de}/{dt}
+        """
+        self.output = (self.Kp * self.PTerm) + (self.Ki * self.ITerm) + (self.Kd * self.DTerm)
+        
 
-            self.output = self.Kp * (self.PTerm + (1.0/self.Ti * self.ITerm) + (self.Td * self.DTerm))
+class PID_standart(PID_ideal):
+    
+    def __init__(self, Kp=0.2, Ti=0.0, Td=0.0, t=0.0):
+        self.Kp = Kp
+        self.Ti = Ti
+        self.Td = Td
+        self.sample_time = 0.0
+        self.current_time = t
+        self.last_time = self.current_time
+        self.clear()
+        
+    def update_output(self):
+        """Calculates PID value for given reference feedback
+        .. math::
+            u(t) = K_p * [ e(t) + 1/T_i \int_{0}^{t} e(t)dt + T_d {de}/{dt} ]
+        """
+        self.output = self.Kp * (self.PTerm + (1.0/self.Ti * self.ITerm) + (self.Td * self.DTerm))
 
 
-class PID_T1(PID_standart):
+class PID_T1(PID_ideal):
+    
     def __init__(self, Kp=0.2, Ti=0.0, Td=0.0, T1=0.0, t=0.0):
         self.Kp = Kp
         self.Ti = Ti
@@ -129,40 +92,10 @@ class PID_T1(PID_standart):
         self.sample_time = 0.00
         self.current_time = t
         self.last_time = self.current_time
-
         self.clear()
         
-    def update(self, t, feedback_value):
-        """Calculates PID value for given reference feedback
-        .. math::
-            u(t) = K_p * [ e(t) + 1/T_i \int_{0}^{t} e(t)dt + T_d {de}/{dt} ]
-        """
-        error = self.SetPoint - feedback_value
-
-        self.current_time = t
-        delta_time = self.current_time - self.last_time
-        delta_error = error - self.last_error
-
-        if (delta_time >= self.sample_time):
-            self.PTerm = error
-            self.ITerm += error * delta_time
-
-            if (self.ITerm < -self.windup_guard):
-                self.ITerm = -self.windup_guard
-                #print 'windup'
-            elif (self.ITerm > self.windup_guard):
-                self.ITerm = self.windup_guard
-                #print 'windup'
-
-            self.DTerm = 0.0
-            if delta_time > 0:
-                self.DTerm = delta_error / delta_time
-
-            # Remember last time and last error for next calculation
-            self.last_time = self.current_time
-            self.last_error = error
-
-            self.output = self.Kp * ((self.T1+self.Ti)/self.Ti*self.PTerm + (1.0/self.Ti * self.ITerm) + (self.T1 + self.Td) * self.DTerm)
+    def update_output(self):
+        self.output = self.Kp * ((self.T1+self.Ti)/self.Ti*self.PTerm + (1.0/self.Ti * self.ITerm) + (self.T1 + self.Td) * self.DTerm)
     
     
     
