b64b863d987c1e134f11b9a4bbe299ffa06a03b3
Voß, Arne <arne.voss@dlr.de>
2020-08-24 17:24:35 +0200

    Speed improvement in writing nodal loads.

--- loadskernel/auxiliary_output.py
+++ loadskernel/auxiliary_output.py
@@ -171,7 +171,8 @@ class AuxiliaryOutput:
             # This is quite a complicated sorting because the subcases from dyn2stat may contain non-numeric characters. 
             # A "normal" sorting returns an undesired sequence, leading IDs in a non-ascending sequence. This a not allowed by Nastran. 
             subcases_IDs = list(self.dyn2stat_data['subcases_ID'])
-            crit_ids = [self.dyn2stat_data['subcases_ID'][list(self.dyn2stat_data['subcases']).index(str(crit_trimcase))] for crit_trimcase in np.unique(self.crit_trimcases) ]
+            subcases = list(self.dyn2stat_data['subcases'])
+            crit_ids = [subcases_IDs[subcases.index(str(crit_trimcase))] for crit_trimcase in np.unique(self.crit_trimcases) ]
             crit_ids = np.sort(crit_ids)
             with open(filename+'_Pg', 'w') as fid: 
                 for subcase_ID in crit_ids:
