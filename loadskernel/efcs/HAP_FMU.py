6176e4531ae44a85685406477e8537a762d8bab0
Voß, Arne <arne.voss@dlr.de>
2020-09-16 15:29:19 +0200

    HAP EFCS: - get all control commands as inputs (incl. stabilizer and flap setting) - apply limits to control surface deflections

--- loadskernel/efcs/HAP_FMU.py
+++ loadskernel/efcs/HAP_FMU.py
@@ -61,20 +61,35 @@ class Efcs(HAP.Efcs):
         self.thrust_actuator = PID.PID_ideal(Kp = 30.0, Ki = 0.0, Kd = 0.0, t=0.0)
         
         # set up limits
-#         self.max_eta  = +20.0/180.0*np.pi
-#         self.min_eta  = -10.0/180.0*np.pi
-#         self.max_zeta = +15.0/180.0*np.pi
-#         self.min_zeta = -15.0/180.0*np.pi
-#         if tas2eas(setpoint_v, setpoint_h) > 15.0:
-#             # limit xi to 1/3 for VNE
-#             self.max_xi   = +5.0/180.0*np.pi
-#             self.min_xi   = -5.0/180.0*np.pi
-#         else:
-#             self.max_xi   = +15.0/180.0*np.pi
-#             self.min_xi   = -15.0/180.0*np.pi
-#         # for all actuators except thrust
+        self.max_eta  = +20.0/180.0*np.pi
+        self.min_eta  = -10.0/180.0*np.pi
+        self.max_zeta = +15.0/180.0*np.pi
+        self.min_zeta = -15.0/180.0*np.pi
+        if tas2eas(setpoint_v, setpoint_h) > 15.0:
+            # limit xi to 1/3 for VNE
+            self.max_xi   = +5.0/180.0*np.pi
+            self.min_xi   = -5.0/180.0*np.pi
+        else:
+            self.max_xi   = +15.0/180.0*np.pi
+            self.min_xi   = -15.0/180.0*np.pi
+        # for all actuators except thrust
         self.max_actuator_speed = 3000.0/180.0*np.pi # 30 rad/s entspricht ca. 5 Hz, Wert von Christian Weiser, 01.07.2020
-    
+        """
+        Mail from Christian, 16.09.2020:
+        %% CTRL SURFACE LIMITS
+        surf_limit(1) =  -10*pi/180; % de_min
+        surf_limit(2) =   10*pi/180; %  de_max
+        surf_limit(3) =  -15*pi/180; % da_min
+        surf_limit(4) =   15*pi/180; % da_max
+        surf_limit(5) =  -15*pi/180; % dr_min
+        surf_limit(6) =   15*pi/180; % dr_max
+        """
+        # apply limits to the FMU, careful with sign conventions!
+        self.fmi.set_real([self.reference_values['surf_limit[1]'], self.reference_values['surf_limit[2]']], [-self.max_eta, -self.min_eta ])
+        self.fmi.set_real([self.reference_values['surf_limit[3]'], self.reference_values['surf_limit[4]']], [ self.min_xi,   self.max_xi  ])
+        self.fmi.set_real([self.reference_values['surf_limit[5]'], self.reference_values['surf_limit[6]']], [-self.max_zeta,-self.min_zeta])
+        
+        
     def controller(self, t, feedback):
           
         dt_fmu = t - self.last_time
@@ -100,16 +115,12 @@ class Efcs(HAP.Efcs):
         command_eta, command_zeta, thrust_l, thrust_r, command_xi = self.fmi.get_real([self.reference_values['y_out[1]'], self.reference_values['y_out[2]'],
                                                                                        self.reference_values['y_out[3]'], self.reference_values['y_out[4]'],
                                                                                        self.reference_values['y_out[5]']])       
-        # Add the trim commands (unknown to the FMU), adjust sign conventions, apply limits.
-#         command_xi   = self.apply_limit( command_xi + self.command_0[0], self.max_xi, self.min_xi) 
-#         command_eta  = self.apply_limit(-command_eta  + self.command_0[1], self.max_eta, self.min_eta)
-#         command_zeta = self.apply_limit(-command_zeta + self.command_0[2], self.max_zeta, self.min_zeta) 
+        # Add the trim commands (unknown to the FMU), adjust sign conventions
         command_xi   =  command_xi   + self.command_0[0]
         command_eta  = -command_eta  + self.command_0[1]
         command_zeta = -command_zeta + self.command_0[2]
         # The FMU distinguishes left and right engine but there is (currently) only one thrust command in loads kernel.
         # For now, take a mean value for both engines.
-        thrust_l, thrust_r = self.fmi.get_real([self.reference_values['y_out[3]'], self.reference_values['y_out[4]']])
         command_thrust = 0.5*(thrust_l + thrust_r) + self.command_0[3]
         
         # Perform time step of actuators and apply limits.
@@ -130,7 +141,8 @@ class Efcs(HAP.Efcs):
         command_dthrust = self.thrust_actuator.output 
  
         # assemble command derivatives in correct order for loads kernel
-        dcommand = np.array([command_dxi, command_deta, command_dzeta, command_dthrust])
+        dcommand = np.zeros(self.command_0.shape)
+        dcommand[0:4] = command_dxi, command_deta, command_dzeta, command_dthrust
 
 #         # Actuator Rates available on LogOut [1:4] = {ddE, ddR, ddT, ddA}  
 #         command_deta, command_dzeta, command_dthrust, command_dxi = self.fmi.get_real([self.reference_values['Log_out[1]'], self.reference_values['Log_out[2]'], 
