6a7383845c1f115056594c02511e351234eb71c0
Voß, Arne <arne.voss@dlr.de>
2021-05-06 16:29:39 +0200

    Switched tests of results to a comparison of hdf5 instead of pickle files for the responses, monstations and dyn2stat.

--- tests/test_loadskernel.py
+++ tests/test_loadskernel.py
@@ -11,81 +11,76 @@ import loadskernel.io_functions as io_functions
 from tests.helper_functions import HelperFunctions
 
 @pytest.fixture(scope='class')
-def _initTestDir(tmpdir_factory):
-    testDir = tmpdir_factory.mktemp('output')
-    testDir = io_functions.specific_functions.check_path(testDir)
-    logging.info("Output directory: %s" % testDir)
-    return str(testDir)
-    #return '/work/voss_ar/loads-kernel-examples/Discus2c/reference_output_escher/'
+def get_test_dir(tmpdir_factory):
+    test_dir = tmpdir_factory.mktemp('output')
+    test_dir = io_functions.specific_functions.check_path(test_dir)
+    return str(test_dir)
 
 @pytest.mark.incremental
 class TestDiscus2c(HelperFunctions):
     job_name = 'jcl_Discus2c'
-    path_input = '/work/voss_ar/loads-kernel-examples/Discus2c/JCLs/'
-    path_reference='/work/voss_ar/loads-kernel-examples/Discus2c/reference_output/'
-        
-    def test_preprocessing_functional(self, _initTestDir):
+    path_input = '/scratch/loads-kernel-examples/Discus2c/JCLs/'
+    path_reference='/work/voss_ar/loads-kernel-examples/reference_output/'
+
+    def test_preprocessing_functional(self, get_test_dir):
         # Here you launch the Loads Kernel with your job
         k = kernel.Kernel(self.job_name, pre=True, main=False, post=False, parallel=False,
                           path_input=self.path_input,
-                          path_output=_initTestDir)
+                          path_output=get_test_dir)
         k.run()
 
-    def test_mainprocessing_functional(self, _initTestDir):
+    def test_mainprocessing_functional(self, get_test_dir):
         # Here you launch the Loads Kernel with your job
         k = kernel.Kernel(self.job_name, pre=False, main=True, post=False, parallel=False,
                           path_input=self.path_input,
-                          path_output=_initTestDir)
+                          path_output=get_test_dir)
         k.run()
 
-    def test_postprocessing_functional(self, _initTestDir):
+    def test_postprocessing_functional(self, get_test_dir):
         # Here you launch the Loads Kernel with your job
         k = kernel.Kernel(self.job_name, pre=False, main=False, post=True, parallel=False,
                           path_input=self.path_input,
-                          path_output=_initTestDir)
+                          path_output=get_test_dir)
         k.run()
-    
-    def test_preprocessing_results(self, _initTestDir):   
+
+    def test_preprocessing_results(self, get_test_dir):
         # do comparisons
         logging.info('Comparing model with reference')
-        model = io_functions.specific_functions.load_model(self.job_name, _initTestDir)
-        del model.path_output # Running the test in a temporary directory means the path_output changes constantly.
+        model = io_functions.specific_functions.load_model(self.job_name, get_test_dir)
         reference_model = io_functions.specific_functions.load_model(self.job_name, self.path_reference)
+        # Running the test in a temporary directory means the path_output changes constantly and can't be compared.
+        del model.path_output
         assert self.compare_dictionaries(model.__dict__, reference_model.__dict__), "model does NOT match reference"
-        
-    def test_mainprocessing_results(self, _initTestDir):
+
+    def test_mainprocessing_results(self, get_test_dir):
         # do comparisons
         logging.info('Comparing response with reference')
-        responses = io_functions.specific_functions.load_responses(self.job_name, _initTestDir, sorted=True)
-        reference_responses = io_functions.specific_functions.load_responses(self.job_name, self.path_reference, sorted=True)
+        responses = io_functions.specific_functions.load_hdf5_responses(self.job_name, get_test_dir)
+        reference_responses = io_functions.specific_functions.load_hdf5_responses(self.job_name, self.path_reference)
         assert self.compare_lists(responses, reference_responses), "response does NOT match reference"
-        
+
         logging.info('Comparing monstations with reference')
-        with open(_initTestDir + 'monstations_' + self.job_name + '.pickle', 'rb') as f:
-            monstations = io_functions.specific_functions.load_pickle(f)
-        with open(self.path_reference + 'monstations_' + self.job_name + '.pickle', 'rb') as f:
-            reference_monstations = io_functions.specific_functions.load_pickle(f)
+        monstations = io_functions.specific_functions.load_hdf5(get_test_dir + 'monstations_' + self.job_name + '.hdf5')
+        reference_monstations = io_functions.specific_functions.load_hdf5(self.path_reference + 'monstations_' + self.job_name + '.hdf5')
         assert self.compare_dictionaries(monstations, reference_monstations), "monstations do NOT match reference"
-        
+
         # do comparisons
         logging.info('Comparing dyn2stat with reference')
-        with open(_initTestDir + 'dyn2stat_' + self.job_name + '.pickle', 'rb') as f:
-            dyn2stat_data = io_functions.specific_functions.load_pickle(f)
-        with open(self.path_reference + 'dyn2stat_' + self.job_name + '.pickle', 'rb') as f:
-            reference_dyn2stat_data = io_functions.specific_functions.load_pickle(f)
+        dyn2stat_data = io_functions.specific_functions.load_hdf5(get_test_dir + 'dyn2stat_' + self.job_name + '.hdf5')
+        reference_dyn2stat_data = io_functions.specific_functions.load_hdf5(self.path_reference + 'dyn2stat_' + self.job_name + '.hdf5')
         assert self.compare_dictionaries(dyn2stat_data, reference_dyn2stat_data), "dyn2stat does NOT match reference"
 
-    def test_postprocessing_results(self, _initTestDir):     
+    def test_postprocessing_results(self, get_test_dir):
         # do comparisons
         logging.info('Comparing crit_trimcases with reference')
-        with open(_initTestDir + 'crit_trimcases_' + self.job_name + '.csv', 'r') as f:
+        with open(get_test_dir + 'crit_trimcases_' + self.job_name + '.csv', 'r') as f:
             lines = f.readlines()
         with open(self.path_reference + 'crit_trimcases_' + self.job_name + '.csv', 'r') as f:
             reference_lines = f.readlines()
         assert self.compare_lists(lines, reference_lines), "crit_trimcases do NOT match reference"
-         
+
         logging.info('Comparing subcases with reference')
-        with open(_initTestDir + 'nodalloads_' + self.job_name + '.bdf_subcases', 'r') as f:
+        with open(get_test_dir + 'nodalloads_' + self.job_name + '.bdf_subcases', 'r') as f:
             lines = f.readlines()
         with open(self.path_reference + 'nodalloads_' + self.job_name + '.bdf_subcases', 'r') as f:
             reference_lines = f.readlines()
@@ -93,20 +88,21 @@ class TestDiscus2c(HelperFunctions):
 
 class TestDiscus2cNonlinSteady(TestDiscus2c):
     job_name = 'jcl_Discus2c_nonlin_steady'
-    path_input = '/work/voss_ar/loads-kernel-examples/Discus2c/JCLs/'
-    path_reference='/work/voss_ar/loads-kernel-examples/Discus2c/reference_output/'
- 
+    path_input = '/scratch/loads-kernel-examples/Discus2c/JCLs/'
+    path_reference='/work/voss_ar/loads-kernel-examples/reference_output/'
+
 class TestDiscus2cTimedom(TestDiscus2c):
     job_name = 'jcl_Discus2c_timedom'
-    path_input = '/work/voss_ar/loads-kernel-examples/Discus2c/JCLs/'
-    path_reference='/work/voss_ar/loads-kernel-examples/Discus2c/reference_output/'
+    path_input = '/scratch/loads-kernel-examples/Discus2c/JCLs/'
+    path_reference='/work/voss_ar/loads-kernel-examples/reference_output/'
 
 class TestAllegraTimedom(TestDiscus2c):
     job_name = 'jcl_ALLEGRA_timedom'
-    path_input = '/work/voss_ar/loads-kernel-examples/Allegra/JCLs/'
-    path_reference='/work/voss_ar/loads-kernel-examples/Allegra/reference_output/'
-        
+    path_input = '/scratch/loads-kernel-examples/Allegra/JCLs/'
+    path_reference='/work/voss_ar/loads-kernel-examples/reference_output/'
+
 class TestAllegraFreqdom(TestAllegraTimedom):
     job_name = 'jcl_ALLEGRA_freqdom'
-    path_input = '/work/voss_ar/loads-kernel-examples/Allegra/JCLs/'
-    path_reference='/work/voss_ar/loads-kernel-examples/Allegra/reference_output/'
+    path_input = '/scratch/loads-kernel-examples/Allegra/JCLs/'
+    path_reference='/work/voss_ar/loads-kernel-examples/reference_output/'
+
