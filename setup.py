c58bf1dd3cdef3582409e8a365e49f61fce25b63
Voß, Arne <arne.voss@dlr.de>
2021-05-14 16:36:14 +0200

    Added a setup file.

---
+++ setup.py
@@ -0,0 +1,37 @@
+"""
+Setup file, currently supports:
+
+- installation via "pip install --user -e <repo_url>"
+- installation via "python setup.py develop --user"
+"""
+
+from setuptools import setup, find_packages
+
+def my_setup():
+    setup(name='Loads-Kernel',
+          version='2021.05',
+          description='The Loads Kernel Software allows for the calculation of quasi-steady and dynamic maneuver loads, unsteady gust loads in the time and frequency domain as well as dynamic landing loads based on a generic landing gear module.',
+          url='https://wiki.dlr.de/display/AE/Lastenrechnung%3A+Loads+Kernel',
+          author='Arne Voß',
+          author_email='arne.voss@dlr.de',
+          license='internal use',
+          packages=find_packages(),
+          python_requires='>=3.7',
+          install_requires=[
+                            'Panel-Aero @ git+https://gitlab.dlr.de/loads-kernel/panel-aero.git',
+                            'matplotlib',
+                            'mayavi',
+                            'traits', 
+                            'traitsui', 
+                            'pyface', 
+                            'pyiges @ git+https://github.com/pyvista/pyiges.git',
+                            'numpy',
+                            'scipy',
+                            'psutil',
+                            'pyfmi',
+                            'h5py',
+                            ],
+          )
+
+if __name__ == '__main__':
+    my_setup()
