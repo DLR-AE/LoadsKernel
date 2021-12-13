215ed5d62d6eab17cc1bb337e2bb4fdd9548efad
Voß, Arne <arne.voss@dlr.de>
2021-12-13 17:45:09 +0100

    Use print instead of logging in setup_mpi, because this need to be done before the logging is set-up

--- loadskernel/cfd_interfaces/mpi_helper.py
+++ loadskernel/cfd_interfaces/mpi_helper.py
@@ -1,4 +1,4 @@
-import platform, logging, sys, os, time
+import platform, sys, os, time
 try:
     import mpi4py
 except:
@@ -11,16 +11,16 @@ def setup_mpi(debug=False):
         comm = mpi4py.MPI.COMM_WORLD
         myid = comm.Get_rank()
         if myid == 0:
-            logging.info('MPI interface initialized with {} processes(es).'.format(comm.Get_size()))
+            print('MPI interface initialized with {} processes(es).'.format(comm.Get_size()))
         if debug:
-            logging.debug('This is MPI process {} with PID {} on {}'.format(myid, os.getpid(), platform.node()))
-            logging.debug('Waiting now for 20 seconds...')
+            print('This is MPI process {} with PID {} on {}'.format(myid, os.getpid(), platform.node()))
+            print('Waiting now for 20 seconds...')
             sys.stdout.flush()
             time.sleep(20)
-            logging.debug('... time is up, continuing.')
+            print('... time is up, continuing.')
     else:
         have_mpi = False
         comm = None
         myid = 0
-        logging.info('No MPI interface found/initialized.')
+        print('No MPI interface found/initialized.')
     return have_mpi, comm, myid
