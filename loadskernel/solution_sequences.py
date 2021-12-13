ec387e2ebc486c87f0452511e7c5426f684a0e6c
Voß, Arne <arne.voss@dlr.de>
2021-12-13 17:54:16 +0100

    moved set_modal_states_fix() to trimconditions

--- loadskernel/solution_sequences.py
+++ loadskernel/solution_sequences.py
@@ -294,15 +294,7 @@ class SolutionSequences(TrimConditions):
         else:
             logging.error('Unknown aero method: ' + str(self.jcl.aero['method']))
         
-        # remove modes from trimcond_Y and _Y
-        i_mass = self.model.mass['key'].index(self.trimcase['mass'])
-        n_modes = self.model.mass['n_modes'][i_mass]
-        
-        for i_mode in range(1, n_modes+1):
-            self.trimcond_X[np.where((self.trimcond_X[:,0] == 'Uf'+str(i_mode)))[0][0],1] = 'fix'
-            self.trimcond_X[np.where((self.trimcond_X[:,0] == 'dUf_dt'+str(i_mode)))[0][0],1] = 'fix'
-            self.trimcond_Y[np.where((self.trimcond_Y[:,0] == 'dUf_dt'+str(i_mode)))[0][0],1] = 'fix'
-            self.trimcond_Y[np.where((self.trimcond_Y[:,0] == 'd2Uf_d2t'+str(i_mode)))[0][0],1] = 'fix'
+        self.set_modal_states_fix()
         xfree_0 = np.array(self.trimcond_X[:,2], dtype='float')[np.where((self.trimcond_X[:,1] == 'free'))[0]] # start trim from scratch
         
         if self.trimcase['maneuver'] == 'bypass':
