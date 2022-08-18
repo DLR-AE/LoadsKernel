8e2a83ffe73d0a25eba8b6f7ff61954cfabb099c
Voß, Arne <arne.voss@dlr.de>
2022-08-18 11:26:26 +0200

    Adding missing imports

--- loadskernel/io_functions/read_b2000.py
+++ loadskernel/io_functions/read_b2000.py
@@ -1,3 +1,6 @@
+import numpy as np
+import scipy.sparse as sp
+
 def read_csv(filename, sparse_output=False):
     # use numpy to load comma separated data 
     data = np.loadtxt(filename, comments='#', delimiter=',')
