6523011d5d950ab150f3ad10b53f740abb914d9a
Voß, Arne <arne.voss@dlr.de>
2022-11-08 17:52:39 +0100

    Removing some non-ascii characters

--- loadskernel/engine_interfaces/propeller.py
+++ loadskernel/engine_interfaces/propeller.py
@@ -35,8 +35,8 @@ class PyPropMat4Loads(object):
     - Theta and q are defined a a positive rotation about the y-axis.
     - Psi and r are defined a a positive rotation about the z-axis.
     
-    [1] Rodden, W., and Rose, T., “Propeller/nacelle whirl flutter addition to MSC/nastran,” in 
-    Proceedings of the 1989 MSC World User’s Conference, 1989.
+    [1] Rodden, W., and Rose, T. Propeller/nacelle whirl flutter addition to MSC/nastran, in 
+    Proceedings of the 1989 MSC World User's Conference, 1989.
     
     [2] https://phabricator.ae.go.dlr.de/diffusion/180/
     """
@@ -322,7 +322,7 @@ class VLM4PropLoads(object):
             self.Pk.append(Pk_rbm + Pk_cam)
             # sum the forces at the origin and rotate back into aircraft system
             self.Pmac.append(Tblade2body.dot(self.model.Dkx1.T.dot(Pk_rbm + Pk_cam)))
-            logging.debug('Forces from blade {} at {:>5.1f}°: {:> 8.1f} {:> 8.1f} {:> 8.1f} {:> 8.1f} {:> 8.1f} {:> 8.1f}'.format(
+            logging.debug('Forces from blade {} at {:>5.1f} : {:> 8.1f} {:> 8.1f} {:> 8.1f} {:> 8.1f} {:> 8.1f} {:> 8.1f}'.format(
                 i_blade, phi_i/np.pi*180.0, 
                 self.Pmac[i_blade][0], self.Pmac[i_blade][1], self.Pmac[i_blade][2], 
                 self.Pmac[i_blade][3], self.Pmac[i_blade][4], self.Pmac[i_blade][5], ))
