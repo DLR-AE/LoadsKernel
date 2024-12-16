2d95955b4c6388838aa3fe0076a3aca3e2f0eed2
Arne Voß <arne.voss@dlr.de>
2024-12-16 17:52:20 +0100

    First try for the MACXP criterion, which still yields unreasonably high values instead of the expected range of 0 to 1.0.

--- loadskernel/fem_interfaces/fem_helper.py
+++ loadskernel/fem_interfaces/fem_helper.py
@@ -29,6 +29,41 @@ def calc_MAC(X, Y, plot=False):
         return MAC, plt
     return MAC
 
+def calc_MACXP(lam_1, my_1, lam_2, my_2, plot=False):
+    """
+    This is function vectorizes the calculation of the extendet modal assurance criterion (MACXP), see for examle
+    equation 28 in [1].
+
+    Nomenklature:
+    eigenvalues = lam_1,2
+    eigenvectors = my_1,2
+
+    [1] Vacher, P., Jacquier, B., and Bucharles, A., “Extensions of the MAC criterion to complex mode”, in Proceedings of the
+    international conference on noise and vibration engineering., Leuven, Belgium, 2010.
+    """
+    # Create an empty matrix
+    MAC = np.zeros((my_1.shape[1], my_2.shape[1]))
+    # Pre-compute the terms in the nominator and denominator
+    nominator = np.abs(my_1.conj().T.dot(my_2)) / np.abs(np.conj(lam_1) + lam_2) \
+        + np.abs(my_1.T.dot(my_2)) / np.abs(lam_1 + lam_2)
+    denom1 = np.abs(my_1.conj().T.dot(my_1)) / (2.0 * np.abs(lam_1.real)) \
+        + np.abs(my_1.T.dot(my_1)) / (2.0 * np.abs(lam_1))
+    denom2 = np.abs(my_2.conj().T.dot(my_2)) / (2.0 * np.abs(lam_2.real)) \
+        + np.abs(my_2.T.dot(my_2)) / (2.0 * np.abs(lam_2))
+    # Loop over the number of modes of the second eigenvector and calculate the MAC values per column.
+    # This loop is necessary in case the size of the the eigenvectors differs.
+
+    MAC = nominator ** 2.0 / denom1 / denom2
+    # Optionally, vizualize the results
+    if plot:
+        plt.figure()
+        plt.pcolor(MAC, cmap='hot_r')
+        plt.colorbar()
+        plt.grid('on')
+
+        return MAC, plt
+    return MAC
+
 
 def force_matrix_symmetry(matrix):
     return (matrix + matrix.T) / 2.0
