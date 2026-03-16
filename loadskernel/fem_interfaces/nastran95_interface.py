0030ffa132eaa04352ce8f2872c365cf28caf67c
Arne Voß <arne.voss@dlr.de>
2026-03-16 14:15:09 +0100

    Switch to internal op2 reader

--- loadskernel/fem_interfaces/nastran95_interface.py
+++ loadskernel/fem_interfaces/nastran95_interface.py
@@ -1,20 +1,12 @@
 # Built-ins
 import logging
-import sys
 
 # Libs
 from scipy.sparse import csc_matrix
 
 # Own modules
 from loadskernel.fem_interfaces.nastran_interface import NastranInterface
-
-# In case a user has only the core packages and no extras installed, we want to avoid an import error when importing the
-# Nastran95Interface class. The OP2 read is only performed when the stiffness or mass matrix is requested, so the error
-# is not relevant until then. In that case, we catch the error and issue an error message.
-try:
-    from pyNastran.op2.op2 import OP2
-except ImportError:
-    pass
+from loadskernel.io_functions import read_op2
 
 
 class Nastran95Interface(NastranInterface):
@@ -25,31 +17,31 @@ class Nastran95Interface(NastranInterface):
         self.GM: csc_matrix = csc_matrix((0, 0))
         self.MGG: csc_matrix = csc_matrix((0, 0))
         self.i_mass: int = 0
-        # Check if OP2 from pyNastran was imported successfully, see try/except statement in the import section.
-        if "pyNastran" not in sys.modules:
-            logging.error(
-                'pyNastran was/could NOT be imported!'
-                'The Nastran95Interface will not be able to read the stiffness and mass matrices from the OP2 file. '
-                'Please install pyNastran or the Loads Kernel with the extras to use this feature.'
-            )
+        self.op2_geom = None
 
     def get_stiffness_matrix(self):
-        op2_model = OP2()
-        try:
-            op2_model.read_op2(self.jcl.geom['filename_op2'])
-        except Exception as e:
-            logging.warning("An error occurred during OP2 read but was ignored.\n%s", e)
-        self.KGG = csc_matrix(op2_model.matrices['KGG'].data)
-        self.GM = csc_matrix(op2_model.matrices['GM'].data).T
+        # Use internal op2 reader. The matrices are numerically eqivalent compared with pyNastran's op2 reader (tested with the
+        # DC3 model using np.allclose).
+        if 'filename_op2' in self.jcl.geom:
+            self.op2_geom = read_op2.read_op2(self.jcl.geom['filename_op2'])
+            # Convert to sparse foramt.
+            self.KGG = csc_matrix(self.op2_geom['KGG'])
+            self.GM = csc_matrix(self.op2_geom['GM'].T)
+        else:
+            logging.error('Please provide filename of .op2 file containing matrices Kgg and GM.')
 
     def get_mass_matrix(self, i_mass):
         self.i_mass = i_mass
-
-        op2_model = OP2()
-        try:
-            op2_model.read_op2(self.jcl.geom['filename_op2'])
-        except Exception as e:
-            logging.warning("An error occurred during OP2 read but was ignored.\n%s", e)
-        self.MGG = csc_matrix(op2_model.matrices['MGG'].data)
+        if 'filename_op2' in self.jcl.mass:
+            op2_mass = read_op2.read_op2(self.jcl.mass['filename_op2'][self.i_mass])
+            self.MGG = csc_matrix(op2_mass['MGG'])
+        else:
+            logging.error('Please provide filename(s) of .op2 files containing the matrices Mgg.')
 
         return self.MGG
+
+    def get_dofs(self):
+        # See if a USET table was included in the OP2 file from the geom setion (along with the stiffness matrix).
+        if self.op2_geom['uset'] is None:
+            logging.error('No USET found in OP2-file %s !', self.jcl.geom['filename_op2'])
+        self.get_sets_from_bitposes(self.op2_geom['uset'])
