248d686b4f744b32c083ebf63a4659dd0bf24465
Arne Voß <arne.voss@dlr.de>
2026-03-31 09:36:33 +0200

    Undo chages to response as it would change all responses in the long-term continuous intergration tests

--- loadskernel/program_flow.py
+++ loadskernel/program_flow.py
@@ -205,11 +205,8 @@ class Kernel(ProgramFlowHelper):
         # Collect response from solution sequence, then destroy it to free memory.
         response = solution_i.response
         response['successful'] = solution_i.successful
-        del solution_i
-        # Add information regarding subcase and description to the response.
         response['i'] = i
-        response['desc'] = jcl.trimcase[i]['desc']
-        response['subcase'] = jcl.trimcase[i]['subcase']
+        del solution_i
         return response
 
     def run_main_sequential(self):
